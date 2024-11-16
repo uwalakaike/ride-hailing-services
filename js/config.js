f73982a0572879635e2b56ddbadaf34239656b24
uwalakaike <uwalakaike1@protonmail.com>
2024-11-16 16:44:06 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_XThp7dgp0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4u0j876j82dob4tt7d86jhsjro', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
